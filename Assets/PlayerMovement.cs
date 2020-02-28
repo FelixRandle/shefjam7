33780eefda0435fd3fe259182f302c57d621895c
cclear1 <61599686+cclear1@users.noreply.github.com>
2020-02-28 22:52:50 +0000

    Added character controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Move character
+        controller.Move(horizontalMove, false, jump);
+        jump = false;
+    }
+}
